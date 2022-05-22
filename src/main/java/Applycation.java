15acbf7938914f77a890577ee8fcdbcad7d8c04c
PontiyRump <popovplay@mail.ru>
2022-05-22 12:20:38 +0300

    Initial commit

---
+++ src/main/java/Applycation.java
@@ -0,0 +1,15 @@
+public class Applycation {
+
+    public static void main(String[] args) {
+        String delimeter = " ";
+        String firstWord = "My";
+        String seconWord = "name";
+        String thirdWord = "is";
+        String fouthWord = "Mikhail";
+
+        String result =
+                firstWord + delimeter + seconWord + delimeter + thirdWord + delimeter + fouthWord;
+        System.out.println(result);
+
+    }
+}
